4934fb5b9eca4738662bf5d6688faae1aaf5f969
zhangtingting <theresword@163.com>
2021-10-18 17:16:32 +0800

    新增了dao的微服务工程

---
+++ cloud-dao/src/main/java/com/ztt/dao/clouddao/CloudDaoApplication.java
@@ -0,0 +1,13 @@
+package com.ztt.dao.clouddao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDaoApplication.class, args);
+    }
+
+}
