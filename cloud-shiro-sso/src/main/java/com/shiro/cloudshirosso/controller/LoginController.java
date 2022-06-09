ac729a5fb4fcf4c54744b678f132c6bb0dbe5126
张廷廷 <theresword@163.com>
2022-06-09 17:53:37 +0800

    生成token的同时保存到redis里面

--- cloud-shiro-sso/src/main/java/com/shiro/cloudshirosso/controller/LoginController.java
+++ cloud-shiro-sso/src/main/java/com/shiro/cloudshirosso/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.shiro.cloudshirosso.controller;
 
+import com.shiro.cloudshirosso.constant.RedisConstant;
 import com.shiro.cloudshirosso.entity.UserInfo;
 import com.shiro.cloudshirosso.jpa.repositories.UserInfoRepositories;
 import com.shiro.cloudshirosso.shiro.utils.JWTUtil;
+import com.shiro.cloudshirosso.utils.RedisTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,25 @@ import java.util.HashMap;
 @RequestMapping
 public class LoginController {
 
+    private UserInfoRepositories userInfoRepositories;
+
     @Autowired
-    UserInfoRepositories userInfoRepositories;
+    public void setUserInfoRepositories(UserInfoRepositories userInfoRepositories) {
+        this.userInfoRepositories = userInfoRepositories;
+    }
 
     @GetMapping("login")
     public String login() {
         HashMap<String, String> claim = new HashMap<>();
-        UserInfo userInfo = userInfoRepositories.findUserInfoByUserNameAndPassword("zhangtt", "2FLy1yE9x_iRz8EOwa-we");
+        UserInfo userInfo = userInfoRepositories.findUserInfoByUserNameAndPassword("gJVpJW-C_x8PnBs2FqA22", "2FLy1yE9x_iRz8EOwa-we");
         if (!ObjectUtils.isEmpty(userInfo)) {
             claim.put("userId", userInfo.getId().toString());
-            return JWTUtil.sign(claim);
+            claim.put("userName", userInfo.getUserName());
+            String sign = JWTUtil.sign(claim);
+            String jti = JWTUtil.getJTI(sign);
+            RedisTool.setKey(RedisConstant.USER_JWT_PREFIX + jti, sign);
+            return sign;
         }
-        return null;
+        return "登录失败!用户名或密码错误.";
     }
 }
