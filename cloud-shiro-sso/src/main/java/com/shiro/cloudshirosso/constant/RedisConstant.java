ca3dc77f35bbfd3b6c00d698963f4c57c401f538
张廷廷 <theresword@163.com>
2022-06-09 17:52:37 +0800

    redis前缀常量

---
+++ cloud-shiro-sso/src/main/java/com/shiro/cloudshirosso/constant/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.shiro.cloudshirosso.constant;
+
+public interface RedisConstant {
+
+    String USER_JWT_PREFIX = "sso:sys.user.jwt:";
+
+    String USER_ROLE_PREFIX = "sso:sys.user.role:";
+
+    String USER_PER_PREFIX = "sso:sys.user.per:";
+
+}
